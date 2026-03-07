da37333c6e4802d9f6eaad11601eb59e773643c2
Alex19barrero <alex.barrero@uniminuto.edu.co>
2026-03-06 21:13:18 -0500

    Create puntos.py

---
+++ puntos.py
@@ -0,0 +1,13 @@
+#algoritmo
+# 1 pedir primer puntaje
+# 2 pedir el segundo codigo
+# comparar puntaje 1 con puntaje 2
+
+#pseudocodigo
+#inicio
+#leer puntaje1
+#leer puntaje2
+#escribir si puntaje1 es mayor que puntaje2
+
+millos=int(input("ingrese el primer puntaje: "))
+nacional=int(input("ingrese el segundo puntaj:"))
